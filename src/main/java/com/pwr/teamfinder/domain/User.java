31c54be93c3e954ef0831e10d17e9e99b0aba3c9
supersyn <supersyn1234@gmail.com>
2014-10-12 12:43:45 +0200

    Added some more columns

--- src/main/java/com/pwr/teamfinder/domain/User.java
+++ src/main/java/com/pwr/teamfinder/domain/User.java
@@ -17,6 +17,38 @@ public class User {
     @NotNull
     private String email;
 
+    @Column(name = "password", nullable = false)
+    @NotNull
+    private String password;
+
+    @Column(name = "city") //nullable or not??
+    @NotNull
+    private String city;
+
+    @Column(name = "street", nullable = false)
+    @NotNull
+    private String street;
+
+    @Column(name = "number", nullable = false)
+    @NotNull
+    private Short number;
+
+    @Column(name = "role", nullable = false)
+    @NotNull
+    private Byte role;
+
+    @Column(name = "name", nullable = false) //nullable or not??
+    @NotNull
+    private String name;
+
+    @Column(name = "surname", nullable = false) //nullable or not??
+    @NotNull
+    private String surname;
+
+    @Column(name = "about")
+    @NotNull
+    private String about;
+
     public Long getId() {
         return id;
     }
@@ -31,5 +63,69 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Short getNumber() {
+        return number;
+    }
+
+    public void setNumber(Short number) {
+        this.number = number;
+    }
+
+    public Byte getRole() {
+        return role;
+    }
+
+    public void setRole(Byte role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
     }
 }
