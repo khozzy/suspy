2bd6d33984d8886a366e3ab265b39a5a48fcc560
supersyn <supersyn1234@gmail.com>
2014-10-22 11:52:17 +0200

    Polskie znaki

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,5 +6,5 @@ ALTER TABLE user CONVERT TO CHARACTER SET utf8 COLLATE utf8_polish_ci;
 ## Users
 INSERT INTO user (id, created_date, name, surname, email, password, role, city, street, house_number, about) VALUES
   (1, NOW(), 'Admin', 'Adminowy', 'admin@niepodam.pl', 'tajne', 'ADMIN', 'Wrocław', 'Sienkiewicza', '30/21', 'Tutaj jakieś informacje o mnie'),
-  (2, NOW(), 'Darek', 'Handlowy', 'fitness@darek.pl', 'koks', 'GYM_OWNER', 'Wrocław', 'Koksownicza', '666', 'Mam siłkę. Chodzcie do mnie');
+  (2, NOW(), 'Darek', 'Handlowy', 'fitness@darek.pl', 'koks', 'GYM_OWNER', 'Wrocław', 'Koksownicza', '666', 'Mam siłkę. Chodźcie do mnie');
 
